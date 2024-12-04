a48f69db86629e3d0295909c15dd8ed418fff01e
Zeynep Merve Koyuncu <sdfd2266@gmail.com>
2024-12-04 21:12:02 +0300

    Refactor activities, update layouts, and fix resource naming conflicts

--- app/src/main/java/com/zeynepmervekoyuncu/instagramclone/MainActivity.java
+++ app/src/main/java/com/zeynepmervekoyuncu/instagramclone/MainActivity.java
@@ -1,32 +1,96 @@
 package com.zeynepmervekoyuncu.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.zeynepmervekoyuncu.instagramclone.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); //findViewById kullanmamak icin
+        View view = binding.getRoot(); //build.gradle.kts'de viewBinding = true yazmamizin sebebi bu
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance(); //firebase documantation'dan bakara yap
+        FirebaseUser user=auth.getCurrentUser(); //uygulamadan cikilinca hesap kapanmasin diye
+
+        if(user != null){
+            Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
-    public void signInClicked(View view){
+    public void signInClicked(View view) {
+        String email = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
 
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Please enter your e-mail and password", Toast.LENGTH_LONG).show();
+        } else {
+            // Kullanıcı giriş işlemi
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    // Başarılı giriş, FeedActivity'e geçiş
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Başarısız giriş
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
+
     public void signUpClicked(View view){
+        String email=binding.emailText.getText().toString();
+        String password=binding.passwordText.getText().toString();
+
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(this,"Please enter your e-mail and password",Toast.LENGTH_LONG).show();
+        }else {
+            //eger basarili bir sekilde email ve password alindiysa:
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this,FeedActivity.class); //mainactivity'den feedActivity'e gideceğiz
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() { //eger basarili degilse
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show(); //getLocalizedMessage kullanicinin anlayacagi dilden bir hata mesaji yazdirir
+                }
+            });
+        }
+
 
     }
 }
--- app/src/main/java/com/zeynepmervekoyuncu/instagramclone/UploadActivity.java
+++ app/src/main/java/com/zeynepmervekoyuncu/instagramclone/UploadActivity.java
@@ -27,6 +27,6 @@ public class UploadActivity extends AppCompatActivity {
 
     }
     public void uploadButtonClicked(View view){
-        
+
     }
 }
